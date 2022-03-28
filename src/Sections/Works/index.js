df4f7074e827c695428208f88440a6683f19d450
Virginia Guiérrez <virginiagutierrez1986@gmail.com>
2022-03-28 01:53:38 -0300

    red arrow added in work section

--- src/Sections/Works/index.js
+++ src/Sections/Works/index.js
@@ -2,6 +2,21 @@ import "./works.css";
 import tapa1 from "../../assets/tapa1.jpg";
 import tapa2 from "../../assets/tapa2.jpg";
 import redArrow from "../../assets/redarrow.png";
+import styled, { keyframes } from "styled-components";
+
+const move = keyframes`
+0% { transform: translateY(-5px)         }
+    50% { transform: translateY(10px)        }
+    100% { transform: translateY(-5px)         }
+`;
+
+const Flebai = styled.div`
+  animation: ${move} 1s ease infinite;
+  @media only Screen and (max-width: 40em) {
+    width: 50vw;
+    padding-bottom: 0;
+  }
+`;
 
 const Works = () => {
   return (
@@ -16,9 +31,9 @@ const Works = () => {
           </p>
           <img id="tapascomic2" src={tapa2} alt="tapa de comic" />
         </div>
-        <div className="redarrow-container">
-          <img src={redArrow} alt="redArrow"></img>
-        </div>
+        <Flebai className="redarrow-container">
+          <img id="flecharoja" src={redArrow} alt="redArrow"></img>
+        </Flebai>
       </div>
     </>
   );
