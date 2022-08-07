0ba53bf224665a64b5d40e296d60b07ac1a2f43c
Virginia Gutiérrez <virginiagutierrez1986@gmail.com>
2022-08-07 15:48:51 -0300

    cambiando el color del footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
-import logo from "../../assets/logo.png";
+// import logo from "../../assets/logo.png";
 
 const FOOTER = styled.footer`
   color: var(--white);
-  background-color: var(--nav2);
+  background-color: #000000;
   padding: 1.2rem calc(2.5rem + 2.5vw);
   font-size: 1rem;
   display: flex;
@@ -20,20 +20,20 @@ const FOOTER = styled.footer`
   }
 `;
 
-const RightText = styled.div`
-  display: flex;
-  align-items: center;
-  img {
-    width: 1.5rem;
-    height: 1.5rem;
-    margin-left: 1rem;
-    transition: all 0.2s ease-in-out;
-  }
-  img:hover {
-    transform: Scale(2);
-    border-radius: 50%;
-  }
-`;
+// const RightText = styled.div`
+//   display: flex;
+//   align-items: center;
+//   img {
+//     width: 1.5rem;
+//     height: 1.5rem;
+//     margin-left: 1rem;
+//     transition: all 0.2s ease-in-out;
+//   }
+//   img:hover {
+//     transform: Scale(2);
+//     border-radius: 50%;
+//   }
+// `;
 const LeftText = styled.div`
   text-align: left;
 `;
@@ -41,10 +41,6 @@ const Footer = () => {
   return (
     <FOOTER id="contact">
       <LeftText>© 2022 Contruido por{" Virginia Gutiérrez"}</LeftText>
-      <RightText>
-        &nbsp;
-        <img src={logo} alt="logo" style={{ width: "30px", height: "30px" }} />
-      </RightText>
     </FOOTER>
   );
 };
